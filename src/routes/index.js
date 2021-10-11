98094cacc76410639c511c7d71d7e8392032b376
Thecots <thecots22@gmail.com>
2021-10-11 21:14:13 +0200

    siuuuuuuuuu

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,18 +4,14 @@ const router = express.Router();
 /* Home */
 router.get("/", (req, res) => {
   res.render("index", {
-    txt: "lorem ipsum sit amet HOME",
-    title: "Home",
-    active: { Home: true },
+    homeCSS: true
   });
 });
 
 /* Servicios */
 router.get("/servicios", (req, res) => {
   res.render("servicios", {
-    txt: "lorem ipsum sit amet CONTACTE",
-    title: "Contacte",
-    active: { Contacte: true },
+    serviciosCSS: true,
   });
 });
 
@@ -23,17 +19,14 @@ router.get("/servicios", (req, res) => {
 router.get("/catalogo", (req, res) => {
   res.render("catalogo", {
     catalogo: true,
-    title: "Contacte",
-    active: { Contacte: true },
+    catalogoCSS: true,
   });
 });
 
 /* Contacto */
 router.get("/contacto", (req, res) => {
   res.render("contacto", {
-    txt: "lorem ipsum sit amet CONTACTE",
-    title: "Contacte",
-    active: { Contacte: true },
+    contactoCSS: true,
   });
 });
 
