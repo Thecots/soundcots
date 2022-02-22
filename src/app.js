481d8b8cd6bd38034517904b9d55e704a7a152cd
Thecots <dcots@insdanielblanxart.cat>
2022-02-22 18:51:53 +0100

    xd

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 const app = express();
 
 // settings
-app.set("port", process.env.PORT || 3030);
+app.set("port", process.env.PORT || 8080);
 app.set("views", path.join(__dirname, "views"));
 
 app.engine(
